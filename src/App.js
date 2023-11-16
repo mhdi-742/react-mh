05b4df1b04870f8513eed80841d42db121bdad1b
mhdi-742 <mhgit7427778@gmail.com>
2023-11-16 19:23:10 +0530

    added features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
+import { useState } from 'react';
 function App() {
+ document.body.style.margin=0;
+ const[mode,SetMode]=useState('light');
+ const[color,SetColor]=useState('black');
+ const[bcolor,SetbColor]=useState('white');
+ const[textareacolor,Ctextareacolor]=useState('white');
+ const change=()=>
+ {
+   if(mode==="light")
+    {SetMode('dark'); SetColor('white');SetbColor('black'); Ctextareacolor('#343a40');}
+   else 
+     {SetMode('light'); SetColor('black');SetbColor('white');Ctextareacolor('white');}  
+ }
+ document.body.style.background=`${bcolor}`;
   return (
     <div className="App">
       <div className='nav'>
-      <Navbar title="CountTxt"></Navbar>
+      <Navbar title="CountTxt" mode={mode} color={color} togle={change}></Navbar>
       </div>
       <div className='ta container'>
-      <TextArea></TextArea>
+      <TextArea mode={mode} color={color} textarea={textareacolor}></TextArea>
       </div>
     </div>
   );
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,8 +1,14 @@
 import { useState } from "react"
 import React from 'react'
-export default function TextArea() {
+export default function TextArea(props) {
     const [text, SetText]=useState('enter your text here');
     const [speedText,ST]=useState('0 min 0 sec');
+    const styl={
+      color: `${props.color}`
+    }
+    const clear=()=>{
+      SetText('');
+    }
     const uppercase=()=>{
         SetText(text.toUpperCase());       
         }
@@ -20,7 +26,19 @@ export default function TextArea() {
         Itext+=text[i].toUpperCase();
       }
       SetText(Itext);
-    }     
+    }    
+    const copy=()=>
+    {
+      const Text=document.getElementById("exampleFormControlTextarea1");
+      Text.select();
+      navigator.clipboard.writeText(text);
+
+    } 
+    const res=()=>
+    {
+      let s=text.split(/[ ]+/);
+      SetText(s.join(" "));
+    }
     const handleOnchange =(event)=>{
         SetText(event.target.value);
         let s=text.split(" ").length*60/238;
@@ -33,22 +51,25 @@ export default function TextArea() {
   return (
     <div>
     <div className="mb-3">
-    <h1>Enter Your Text</h1>
-    <hr></hr>
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="15" value={text} onChange={handleOnchange}></textarea>
+    <h1 style={styl}>Enter Your Text</h1>
+    <hr style={styl}></hr>
+    <textarea className="form-control" id="exampleFormControlTextarea1" rows="15" value={text} onChange={handleOnchange}style={{background:`${props.textarea}`,color:props.color}}></textarea>
    </div>
-   <div>
+   <div className='my-3'>
      <button type="button" className="btn btn-primary mx-3" onClick={uppercase}>UPPERCASE</button>
      <button type="button" className="btn btn-primary mx-3" onClick={lowercase}>LOWERCASE</button>
      <button type="button" className="btn btn-primary" onClick={inversion}>INVERSION</button>
+     <button type="button" className="btn btn-primary mx-3" onClick={copy}>COPY</button>
+     <button type="button" className="btn btn-primary mx-1" onClick={res}>REMOVE EXTRA SPACE</button>
+     <button type="button" className="btn btn-primary mx-1" onClick={clear}>CLEAR</button>
     </div>
     <div className="container my=3">
-       <h2>
+       <h2 style={styl}>
            TEXT SUMMARY
        </h2>
-       <p>Words: {text.split(" ").length} characters: {text.length} Aprox time to read:{speedText}</p>
-       <h2>PREVIEW</h2>
-       <p>{text}</p>
+       <p style={styl}>Words: {text.split(" ").length} characters: {text.length} Aprox time to read:{speedText}</p>
+       <h2 style={styl}>PREVIEW</h2>
+       <p style={styl}>{text}</p>
     </div>
     </div>
   )
